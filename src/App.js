4c99a0ee1633205f1e6a30aaae7fd3e5bcf9d4b6
Simon Garnotel <simon.garnotel@gmail.com>
2021-10-21 15:26:53 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Form, Input, Layout, Modal, Space } from "antd";
+import "antd/dist/antd.compact.css";
 
 function App() {
+  const [form] = Form.useForm();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Modal
+        visible={true}
+        onOk={async () => {
+          try {
+            const values = await form.validateFields();
+            console.log(values);
+            form.resetFields();
+          } catch (err) {}
+        }}
+      >
+        <Form form={form}>
+          <Form.List name="test">
+            {(fields, { add, remove }) => (
+              <>
+                {fields.map(({ key, name, fieldKey, ...restField }) => (
+                  <div key={key}>
+                    <Space>
+                      Parameters {name + 1}
+                      <Button type="danger" onClick={() => remove(name)} />
+                    </Space>
+
+                    <Form.Item
+                      {...restField}
+                      label="first"
+                      name={[name, "first"]}
+                      fieldKey={[fieldKey, "first"]}
+                      rules={[{ required: true }]}
+                    >
+                      <Input />
+                    </Form.Item>
+                    <Form.Item
+                      {...restField}
+                      label="second"
+                      name={[name, "second"]}
+                      fieldKey={[fieldKey, "second"]}
+                      rules={[{ required: true }]}
+                    >
+                      <Input />
+                    </Form.Item>
+                    <Form.Item
+                      {...restField}
+                      label="third"
+                      name={[name, "third"]}
+                      fieldKey={[fieldKey, "third"]}
+                      rules={[{ required: true }]}
+                    >
+                      <Input />
+                    </Form.Item>
+                  </div>
+                ))}
+                <Button onClick={add}>Add</Button>
+              </>
+            )}
+          </Form.List>
+        </Form>
+      </Modal>
+    </Layout>
   );
 }
 
